9c3f08685270bb1cd7bbe32f22498e9fe54ac59a
王蕾 <wanglei@wangleideMacBook-Pro.local>
2021-02-16 18:29:17 +0800

    增加

---
+++ willow-backend/src/main/java/com/nature/willowbackend/Service.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2018 iQIYI.COM - All Rights Reserved.
+ * This file is part of Pizza project. Unauthorized copy
+ * of this file, via any medium is strictly prohibited.
+ * Proprietary and Confidential.
+ *
+ * Author(s): INQ Team <iig_ipd_inq@qiyi.com>
+ */
+package com.nature.willowbackend;
+
+public class Service {
+}
